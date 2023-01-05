3ead27d04a940d5cef1b95d8fe1e93ab19f7566c
monikacoder <120687956+monikacoder@users.noreply.github.com>
2023-01-05 22:29:16 +0000

    Create String-Manipulation.py

---
+++ String-Manipulation.py
@@ -0,0 +1,8 @@
+#TASK-1: Calculate length of string without using inbuilt function
+name = "Monika"
+index = 0
+for letter in name:
+    index = index + 1
+print("the length of string is : " + str(index))
+#-----------------------------
+
